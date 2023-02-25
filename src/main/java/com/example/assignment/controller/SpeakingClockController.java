831dd0fd7753ba69f55ff8abb109c15737b7d888
rachitjha78 <rachitnagar@gmail.com>
2023-02-25 16:47:43 +0530

    controller changes

--- src/main/java/com/example/assignment/controller/SpeakingClockController.java
+++ src/main/java/com/example/assignment/controller/SpeakingClockController.java
@@ -1,20 +1,29 @@
 package com.example.assignment.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.assignment.service.CurrentTimeService;
+
 import java.time.LocalTime;
 
 @RestController
 @RequestMapping(path = "/clock")
+
+
 public class SpeakingClockController {
+
+	@Autowired
+	private CurrentTimeService currentTimeService;
 	
 	@GetMapping(path="/", produces = "application/json")
     public String getTimeInWord()
     {
     
     	LocalTime localtime = LocalTime.now();
-    	return localtime.getHour() + ":" + localtime.getMinute();
+    	return currentTimeService.getCurrentTimeInWord();
         
     }
 
--- src/main/java/com/example/assignment/service/CurrentTimeService.java
+++ src/main/java/com/example/assignment/service/CurrentTimeService.java
@@ -2,6 +2,9 @@ package com.example.assignment.service;
 
 import java.time.LocalTime;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class CurrentTimeService {
 	
 	public String getCurrentTimeInWord() {
